f64e0a319df57c44f83bb8ddcc4f880458145fe6
Widny St. Louis <widnystlouis1@gmail.com>
2015-02-06 16:04:54 -0500

    first commit

---
+++ app/controllers/sample_controller.rb
@@ -0,0 +1,15 @@
+class SampleController < ApplicationController
+
+  # def action
+  #   api_key = 45131832
+  #   api_secret = 1f2e59645867028f4fa5b4e38bbdd759dbb5e6e4
+
+  #   opentok = OpenTok::OpenTok.new api_key, api_secret
+  #   session = opentok.create_session
+  #   session_id = session.session_id
+  #   token = session_id.generate_token 
+
+  # end
+
+
+end
